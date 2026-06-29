3320907c6fef3aee28f1a43614e3a0ed0c5c47e4
Fernando Neto <fernandoneto786@gmail.com>
2026-06-28 22:15:08 -0300

    Arquivo primeira tarefa.

--- Main.py
+++ Main.py
@@ -1,22 +1,35 @@
 from tkinter import *
+from tkinter import ttk
 
 #******* MAIN *******#
 
-# Todas os círuculos desenhados são armazenados aqui
-circulos = []
-raio = None
+figuras = []       # todas as figuras desenhadas
+figura_nova = None # figura que está sendo desenhada
 
 root = Tk()
+root.title("App de Desenho")
+frame = Frame(root)
 
-canvas = Canvas(root, bg='white', width=600, height=600)
-canvas.pack()
+paddings = {'padx': 5, 'pady': 5} 
 
-ini_x = None
-ini_y = None
-fim_x = None
-fim_y = None
-canvas.bind('<ButtonPress-1>', inicia_linha)
-canvas.bind('<B1-Motion>', atualiza_linha)
-canvas.bind('<ButtonRelease-1>', incluir_linha)
+# Label
+label = ttk.Label(frame,  text='Ferramenta:')
+label.grid(column=0, row=0, sticky=W, **paddings)
+
+tipo_figura_var = StringVar(root)
+option_menu = ttk.OptionMenu(frame, tipo_figura_var,
+                             'Linha', 'Linha', 'Rabisco', 'Elipse', 'Círculo', 'Retângulo')
+option_menu.grid(column=1, row=0, sticky=W, **paddings)
+
+# Área de desenho
+canvas = Canvas(frame, bg='white', width=600, height=600)
+canvas.grid(column=0, row=1, columnspan=2, sticky=W, **paddings)
+
+frame.pack()
+
+# Eventos de mouse associados ao canvas
+canvas.bind('<ButtonPress-1>', iniciar_figura_nova)
+canvas.bind('<B1-Motion>', atualizar_figura_nova)
+canvas.bind('<ButtonRelease-1>', incluir_figura_nova)
 
 root.mainloop()
