79d819a0fefe1906925dd4eedd72af062a595e6b
cauanotcoding <cauagabriellima@gmail.com>
2026-07-03 22:02:48 -0300

    Update no arquivo de retangulos

--- retangulos.py
+++ retangulos.py
@@ -1,70 +1,25 @@
-#Fazer retangulos
-#A largura será a reta definida pela distância entre a posição x inicial e final
-#A altura segue a mesma lógica da largura
-
-from tkinter import *
+from figura import Figura
 
 
-# Quando o mouse é pressionado
-def inicia_retangulo(event):
-    global ini_x, ini_y
-    ini_x = event.x
-    ini_y = event.y
+class Retangulo(Figura):
+    def __init__(self,coordenadas, x_inicial, y_inicial, corBorda="black", corPreenchimento="", dash=None):
+        super().__init__(coordenadas, corBorda, corPreenchimento, dash)
+        coordenadas = [x_inicial, y_inicial, x_inicial, y_inicial]
+        
 
+    def desenhar(self, canvas):
+        x1, y1, x2, y2 = self.coordenadas
+        canvas.create_rectangle(
+            x1, y1, x2, y2,
+            outline=self.corBorda,
+            fill=self.corPreenchimento,
+            dash=self.dash
+        )
 
-# Quando o mouse é movido com o botão pressionado
-def atualiza_retangulo(event):
-    global fim_x, fim_y
-    fim_x = event.x
-    fim_y = event.y
-    desenhar()  # redesenha os retângulos já finalizados
-    # Desenha a prévia do retângulo atual
-    x1, y1 = min(ini_x, fim_x), min(ini_y, fim_y)
-    x2, y2 = max(ini_x, fim_x), max(ini_y, fim_y)
-    canvas.create_rectangle(x1, y1, x2, y2)
+    def atualizar(self, event):
+        self.coordenadas[2] = event.x
+        self.coordenadas[3] = event.y
 
-# Quando o mouse é solto
-def finaliza_retangulo(event):
-    global fim_x, fim_y
-    fim_x = event.x
-    fim_y = event.y
-    # Normaliza coordenadas (canto superior-esquerdo e inferior-direito)
-    x1, y1 = min(ini_x, fim_x), min(ini_y, fim_y)
-    x2, y2 = max(ini_x, fim_x), max(ini_y, fim_y)
-    retangulos.append((x1, y1, x2, y2))
-    desenhar()
-
-
-def desenhar():
-    canvas.delete("all")
-    for ret in retangulos:
-        x1, y1, x2, y2 = ret
-        canvas.create_rectangle(x1, y1, x2, y2)
-
-
-
-
-#******* MAIN *******#
-
-
-# Todos os retângulos desenhados são armazenados aqui
-retangulos = []
-
-root = Tk()
-
-
-canvas = Canvas(root, bg='white', width=600, height=600)
-canvas.pack()
-
-
-ini_x = None
-ini_y = None
-fim_x = None
-fim_y = None
-
-
-canvas.bind('<ButtonPress-1>', inicia_retangulo)
-canvas.bind('<B1-Motion>', atualiza_retangulo)
-canvas.bind('<ButtonRelease-1>', finaliza_retangulo)
-
-root.mainloop()
+    def verificarFig(self):
+        x1, y1, x2, y2 = self.coordenadas
+        return abs(x2 - x1) > 2 and abs(y2 - y1) > 2
