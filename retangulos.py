ba43c47f966493691a68ea4fa3b6b68f56b4ac41
cauanotcoding <cauagabriellima@gmail.com>
2026-07-03 23:37:39 -0300

    Adição de comentários no arquivo retângulos
    Fix constructor method name to __init__ for Retangulo class.

--- retangulos.py
+++ retangulos.py
@@ -1,21 +1,27 @@
 from figura import Figura
 from tkinter import *
 
+# Classe filha da Figura
 class Retangulo(Figura):
-    def __init__(self,coordenadas, corBorda, corPreenchimento, dash):
-        super().__init__(coordenadas, corBorda, corPreenchimento, dash)
+    # Inicializa os atributos herdados
+    def _init_(self,coordenadas, corBorda, corPreenchimento, dash):
+        super()._init_(coordenadas, corBorda, corPreenchimento, dash)
         
+    # Desenha o retângulo no canvas
     def desenhar(self, canvas):
         canvas.create_rectangle(self.coordenadas[0], self.coordenadas[1], self.coordenadas[2], self.coordenadas[3], outline= self.corBorda,fill= self.corPreenchimento, dash= self.dash)
 
+    # Atualiza o ponto final da figura
     def atualizar(self, novoX, novoY):
         self.coordenadas = [self.coordenadas[0], self.coordenadas[1], novoX, novoY]
 
+    # Verifica se a figura tem tamanho válido
     def verificarFig(self):
         if abs(self.coordenadas[2] - self.coordenadas[0]) > 2 and abs(self.coordenadas[3] - self.coordenadas[1]) > 2:
             return True
         else:
             return False
 
+    # Finaliza a figura removendo o tracejado
     def finalizar(self):
         self.dash = ''
